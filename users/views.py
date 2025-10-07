8ec2c529dde7daac31280a7322b7cd9e75898dec
MainUser <user@example.com>
2025-10-08 01:15:15 +0700

    настроил фильтрацию payments

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django_filters.rest_framework import DjangoFilterBackend, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework import viewsets
 
 from users.models import Payment
@@ -9,5 +10,5 @@ class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = ['course', 'lesson', 'payment_method']
+    filterset_fields = ['paid_course', 'paid_lesson', 'payment_method']
     ordering_fields = ['payment_date']
