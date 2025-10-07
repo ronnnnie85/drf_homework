7f3f7989d7a68a2da6ac71ab3a5c3d19dc89dfb4
MainUser <user@example.com>
2025-10-07 23:04:22 +0700

    добавил поле вывода количества уроков

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,13 +3,19 @@ from rest_framework import serializers
 from lms.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return obj.lesson_set.count()
+
+    class Meta:
+        model = Course
+        fields = "__all__"
+
