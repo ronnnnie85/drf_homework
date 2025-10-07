317a713c253640e5ff31a2b64d0a314448d7e5b6
MainUser <user@example.com>
2025-10-08 00:47:59 +0700

    добавил кастомную команду

---
+++ users/management/commands/create_payments.py
@@ -0,0 +1,56 @@
+from django.core.management.base import BaseCommand
+from users.models import Payment, User
+from lms.models import Lesson, Course
+from datetime import date
+
+
+class Command(BaseCommand):
+    help = "Создаёт тестовые записи в таблице Payment и удаляет старые записи"
+
+    def handle(self, *args, **kwargs):
+        deleted_count, _ = Payment.objects.all().delete()
+        self.stdout.write(self.style.WARNING(f"🗑 Удалено старых записей: {deleted_count}"))
+
+        user = User.objects.first()
+
+        lessons = Lesson.objects.all()[:2]
+        course = Course.objects.first()
+
+        if not user:
+            self.stdout.write(self.style.ERROR("Нет пользователей в базе"))
+            return
+
+        if lessons.count() < 2:
+            self.stdout.write(self.style.ERROR("Нужно минимум 2 урока в базе"))
+            return
+
+        if not course:
+            self.stdout.write(self.style.ERROR("Нет курсов в базе"))
+            return
+
+        payments = [
+            Payment(
+                user=user,
+                payment_date=date(2025, 10, 1),
+                amount=1500,
+                payment_method='cash',
+                paid_lesson=lessons[0],
+            ),
+            Payment(
+                user=user,
+                payment_date=date(2025, 10, 3),
+                amount=2000,
+                payment_method='transfer',
+                paid_lesson=lessons[1],
+            ),
+            Payment(
+                user=user,
+                payment_date=date(2025, 10, 5),
+                amount=5000,
+                payment_method='transfer',
+                paid_course=course,
+            ),
+        ]
+
+        Payment.objects.bulk_create(payments)
+        self.stdout.write(self.style.SUCCESS("Успешно добавлено 3 записи в таблицу Payment"))
