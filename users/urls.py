febdbaa8e426d82735c4bdd6bbe5a507273ca184
MainUser <user@example.com>
2025-10-26 12:23:52 +0700

    добавил контроллер и эндпоинт

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet, UserCreateApiView, UserViewSet
+from users.views import PaymentViewSet, UserCreateApiView, UserViewSet, PaymentCheckoutCreateAPIView
 
 app_name = UsersConfig.name
 
@@ -15,6 +15,7 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", UserCreateApiView.as_view(), name="user_create"),
+    path("payments/checkout/", PaymentCheckoutCreateAPIView.as_view(), name="payment_checkout"),
 ]
 
 urlpatterns += router.urls
